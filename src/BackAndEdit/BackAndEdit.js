b7b410e4b9e0158c276c4c5932d5bd4412769d4f
Cristian Osorio Bretti <cristian.osoriobretti@gmail.com>
2019-02-28 12:25:03 +0100

    Don't need state on BackAndEdit.js

--- src/BackAndEdit/BackAndEdit.js
+++ src/BackAndEdit/BackAndEdit.js
@@ -3,19 +3,13 @@ import {Link} from 'react-router-dom';
 import Button from '../shared/Button';
 
 const Overview = ({model}) => {
-    const [numberOfGuests, setNumberOfGuests] = useState(model.getNumberOfGuests());
-
-    useEffect(() => {
-        setNumberOfGuests(model.getNumberOfGuests());
-    }, [numberOfGuests])
-
 
     return <div id="backAndEditContainer" className="flex border-b-4 border-black p-3">
             <div className="flex-1 flex lg:text-xl font-bold p-2">
                     <div>
                             My dinner:&nbsp;
                     </div>
-                    <div id="numberOfGuests">{numberOfGuests}</div>
+                    <div id="numberOfGuests">{model.getNumberOfGuests()}</div>
                     <div>
                             &nbsp;people
                     </div>
